e5c4761b7382d41d8e724ebfa8a38fa8c2780d8c
johann <johanngelhorn@gmail.com>
2020-02-04 10:06:53 +0100

    minor tweaks in config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,7 +8,7 @@
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./xserver.nix
-    ./home-manager.nix
+    # ./home-manager.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
